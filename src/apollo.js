24c815f6922b303538573d95798bcee1d1bb9b2b
Caiprozect <caiprozect@gmail.com>
2022-01-22 08:10:17 +0900

    Working...

---
+++ src/apollo.js
@@ -0,0 +1,25 @@
+import { ApolloClient, InMemoryCache } from "@apollo/client";
+
+const client = new ApolloClient({
+  uri: "http://localhost:4000",
+  cache: new InMemoryCache(),
+  resolvers: {
+    Movie: {
+      isLiked: () => false,
+    },
+    Mutation: {
+      toggleLikeMovie: (_, { id, isLiked }, { cache }) => {
+        cache.modify({
+          id: `Movie:${id}`,
+          fields: {
+            isLiked(val) {
+              return !val;
+            },
+          },
+        });
+      },
+    },
+  },
+});
+
+export default client;
